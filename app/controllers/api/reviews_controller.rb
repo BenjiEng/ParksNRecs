069ecc1c9bb670121e5c1fdf2f78d94338e85e96
Ben E <benjamin.eng7@gmail.com>
2015-03-24 14:15:17 -0700

    Reivews controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,20 +1,41 @@
 class Api::ReviewsController < ApplicationController
 
   def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      render json: @review
+    else
+      flash.new[:errors] = @review.errors.full_messages
+    end
+  end
 
+  def show
+    @review = Review.find(params[:id])
+    render :show
   end
 
   def edit
-
+    @review = Review.find(params[:id])
+    if @review.update_attributes(review_params)
+      render :show
+    else
+      render json: @review.errors
+    end
   end
 
   def destroy
-    
+    @review = Review.find(params[:id])
+    @review.destroy!
+    render :show
   end
 
   private
 
     def review_params
+      params.require(:review).permit(:overall_score, :safety_score, :seating_score,
+                                     :run_score, :kid_score, :view_score, :drug_score,
+                                     :comments, :park_id)
     end
 
 end
