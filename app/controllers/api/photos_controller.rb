aa8dab0f623977c5ecb0f2d506b701c8944c2bf8
Ben E <benjamin.eng7@gmail.com>
2015-05-20 14:28:44 -0700

    Photos controller edit

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,20 +1,34 @@
 class Api::PhotosController < ApplicationController
   def create
+    @photo = Photo.new(photo_params)
+    @photo.user_id = current_user.id
+    if @photo.save
+      render json: @photo
+    else
+      render json: @photo.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def index
+    @photos = Photos.all
+    render :index
   end
 
   def show
+    @photo = Photo.find(params[:id])
+    render :show
   end
 
   def destroy
+    @photo = Photo.find(params[:id])
+    @photo.delete
+    render :show
   end
 
   private
 
     def photo_params
-      params.require(:photo).permit(:)
+      params.require(:photo).permit(:user_id, :park_id, :photo_url, :title, :description)
     end
 
 end
