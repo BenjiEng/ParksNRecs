f02ec61e2d458205ab66040f32f1d0aaa5380d5a
Ben E <benjamin.eng7@gmail.com>
2015-07-12 10:08:08 -0700

    fix images error 2

--- app/assets/javascripts/views/photos/imageIndexItem.js
+++ app/assets/javascripts/views/photos/imageIndexItem.js
@@ -6,7 +6,7 @@ ParksNRecs.Views.ImageIndexItem = Backbone.View.extend({
   },
 
   render: function () {
-    var content = this.template({image: this.model});
+    var content = this.template({picture: this.model}); 
     this.$el.html(content);
     return this;
   }
