989b1f084ea495dd0339807c842a0fdbb9831a9e
Ben E <benjamin.eng7@gmail.com>
2015-07-12 13:17:43 -0700

    fix images error 3

--- app/assets/javascripts/views/photos/imageIndexItem.js
+++ app/assets/javascripts/views/photos/imageIndexItem.js
@@ -2,11 +2,12 @@ ParksNRecs.Views.ImageIndexItem = Backbone.View.extend({
   template: JST['photos/image'],
 
   initialize: function() {
-    this.render()
+    this.listenTo(this.model, 'add', this.render);
+    // this.render()
   },
 
   render: function () {
-    var content = this.template({picture: this.model}); 
+    var content = this.template({picture: this.model});
     this.$el.html(content);
     return this;
   }
